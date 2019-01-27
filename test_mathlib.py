e8cf63e7c1a7caa2afae8254bb0869b7b966bed1
Juan Manuel Bertoni <jm_bertoni@yahoo.com.ar>
2019-01-27 14:25:07 -0300

    new files

--- test_mathlib.py
+++ test_mathlib.py
@@ -1,2 +1,14 @@
 import mathlib
+import pytest
 
+@pytest.mark.windows
+def test_calc_total():
+    total = mathlib.calc_total(4,5)
+    assert total == 9
+
+def test_calc_multiply():
+    result = mathlib.calc_multipy(4,5)
+    assert result == 20
+@pytest.mark.linux
+def test_dummy():
+    assert True
